de4805d4624753ad5ba3f0dd9c09247528562ef0
georgejinu <34377862+georgejinu@users.noreply.github.com>
2018-01-07 09:27:17 -0500

    Modified Oauth process

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose')
+const keys = require('./config/keys');
+require('./models/user') 
+require('./services/passport'); 
+const cookieSession = require('cookie-session');
 const passport = require('passport');
-const keys = require('./config/keys')
-const GoogleStrategy = require('passport-google-oauth20');
-const app = express();
- 
-passport.use(new GoogleStrategy({ 
-    clientID : keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-}, accessToken => {
-    console.log(accessToken);
-}));
 
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}));
+mongoose.connect(keys.mongoURI); 
+const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookiesKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
