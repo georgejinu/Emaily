9ac0ecda05472f01f81e8bb1e0a4747bb75d0537
georgejinu <34377862+georgejinu@users.noreply.github.com>
2018-01-07 10:54:58 -0500

    refactoring config

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
-module.exports = {
-  googleClientID: "181377120104-6q9glc9kqev6mj1c74mc3k7kvf270flf.apps.googleusercontent.com",
-  googleClientSecret: "YGDGpXfSfH8tWFbRL7lpuDPQ",
-  mongoURI: 'mongodb://JinuGeorge:Welcome123#@ds245277.mlab.com:45277/emaily-dev-vo',
-  cookiesKey: 'SDFSDFDSFSDFQWEWQEWQELJJQWLEJLQWEJLQWJELJQWGKJGWEIQWGEW'
-};
+if(process.env.NODE_ENV === 'production') {
+  module.exports = require('./prod');
+}
+else {
+  module.exports = require('./dev');
+}
